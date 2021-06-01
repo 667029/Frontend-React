e838008535dec54ac9bf855d3dda49a0e54f0dad
Asiel Alvarez <asielalvarez@ufl.edu>
2021-05-31 21:22:33 -0400

    Remove warnings

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authenticationSlice from './authenticationSlice';
 import expensesSlice from './expensesSlice';
 import ToastMiddleware from '../middlewares/ToastMiddleware';
